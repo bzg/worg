bde13e5be0297a5285a13e69e7f9e4d33ed3c6c0
David Arroyo Menendez <davidam@gnu.org>
2015-05-03 19:28:13 +0200

    orgguide: delete make.sh clean.sh

--- orgguide/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-rm orgguide.es.pg orgguide.es.tp orgguide.es.ky orgguide.es.vr orgguide.es.cp orgguide.es.log orgguide.es.toc orgguide.es.fn orgguide.es.aux
--- orgguide/make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-texi2html orgguide.es.texi
-pdftex orgguide.es.texi
