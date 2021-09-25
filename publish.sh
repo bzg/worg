10e3943808fa2ca30f869e2d8625bef7ce816908
Bastien <bzg@gnu.org>
2021-09-25 11:43:42 +0200

    Publish through SourceHut build
    Add .build.yml and publish.sh for publication through SourceHut build.

---
+++ publish.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+":"; exec emacs --quick --script "$0" -- "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
+
+(add-to-list 'load-path "~/org-mode/lisp/")
+(require 'ox-html)
+(load "/usr/share/emacs/site-lisp/elpa-src/htmlize-1.55/htmlize.el")
+
+(setq make-backup-files nil
+      debug-on-error t)
+
+(setq org-confirm-babel-evaluate nil
+      org-html-style-default ""
+      org-html-scripts ""
+      org-html-htmlize-output-type 'css
+      org-html-doctype "html5"
+      org-html-html5-fancy t
+      org-html-validation-link nil
+      org-html-postamble nil)
+
+(dolist (org-file (directory-files-recursively default-directory "\\.org$"))
+  (let ((html-file (concat (file-name-directory org-file)
+			   (file-name-base org-file) ".html")))
+    (if (and (file-exists-p html-file)
+             (file-newer-than-file-p html-file org-file))
+	(message " [skipping] unchanged %s" org-file)
+      (message "[exporting] %s" (file-relative-name org-file default-directory))
+      (with-current-buffer (find-file org-file)
+	(condition-case err
+            (org-html-export-to-html)
+          (error (message (error-message-string err))))))))
