650cce9a1d25b10927e9a2af7b9f2d06132366e4
Suhail Singh <suhailsingh247@gmail.com>
2024-12-03 16:43:54 -0500

    .dir-locals.el: Disable byte-compilation
    Since 2022-10-05, when adding directory-local-variables, the
    no-byte-compile cookie gets set.  I believe this change was introduced
    in response to Emacs bug#58486.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
 ((nil
   (indent-tabs-mode . t)
   (tab-width . 8)
